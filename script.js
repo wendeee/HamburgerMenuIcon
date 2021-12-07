55480fbf3b0f5e763b0c78f8a2a64135dd9b2081
Chinwendu <enyinnapc16127@gmail.com>
2021-12-07 07:55:42 -0800

    Created an animated hamburger menu icon

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenuIcon(){
+    menuIcon.classList.toggle('active');
+}
+menuIcon.addEventListener('click', toggleMenuIcon);
